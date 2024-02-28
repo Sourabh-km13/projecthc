ed0d2e428c751fb746c86f38bd5044d15bde5cd0
Sourabh-km13 <157295892+Sourabh-km13@users.noreply.github.com>
2024-02-28 16:15:28 +0000

    js added

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+const a= document.querySelector('.tags>div>button');
+const info=document.querySelector('.info');
+const cards=document.querySelector('.cards')
+a.addEventListener('click',function(e){
+    info.style.filter='blur(8px)'
+    cards.style.filter='blur(8px)'
+    
+    
+})
+  
